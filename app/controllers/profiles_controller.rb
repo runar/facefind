080684883f1524d855338b6697004ee165941f81
Runar <runar@me.com>
2010-06-10 13:33:17 +0200

    Redirects to root on empty/no id and validates url against regexes.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,7 +11,19 @@ class ProfilesController < ApplicationController
   end
   
   def find
+    # Redirect to root path unless id exists
+    redirect_to root_path and return unless params[:id]
     
+    # Find Facebook if from url (id param) by using regexes
+    profile_id = case params[:id]
+    when /[a-z\/:\.-]+\.fbcdn\.net\/[\w\/\-]+\/\w(\d+)_[\d\_]+.jpg/ then "#{$1}"
+    when /[a-z\/:\.-]+\.fbcdn\.net\/[\w\/\-\.]+\/\d*_?\d+_(\d+)_\d+_[\d_]*\w.jpg/ then "#{$1}"
+    when /[a-z\/:\.]*\.facebook\.com\/[\w\W]+\.php\?pid=\d+&id=(\d+)(&fbid=\d+)?/ then "#{$1}"
+    
+    # No matches, show error
+    else
+      handle_error('Wrong format!') and return
+    end
   end
 
 end
