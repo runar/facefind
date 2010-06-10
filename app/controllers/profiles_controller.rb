61cd51bf8335cf20c178ebce018f67e998a746bf
Runar <runar@me.com>
2010-06-10 13:26:42 +0200

    Added search method with temp redirects.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,16 @@ class ProfilesController < ApplicationController
   def index
   end
 
+  def search
+    # Redirect to root path unless id exists
+    redirect_to root_path and return unless params[:id]
+    
+    # Redirect to find action to show data
+    redirect_to find_path(:id => params[:id])
+  end
+  
   def find
+    
   end
 
 end
