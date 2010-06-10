579fb842a42a3e7ad00f8103a5bee5ab5d16718e
Runar <runar@me.com>
2010-06-10 13:39:33 +0200

    Changed find route to allow special characters (\w and \W) in id param.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Facefind::Application.routes.draw do |map|
   
   # Find profile route
-  match '/find(.:format)(/:id)' => "profiles#find", :as => :find
+  match '/find(.:format)(/:id)' => "profiles#find", :id => /[\w\W]+/, :as => :find
   
   # Search route
   match '/search' => "profiles#search", :as => :search
