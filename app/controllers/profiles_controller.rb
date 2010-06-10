69e8390750d6dc35981448393a4de99b1f1d67b6
Runar <runar@me.com>
2010-06-10 14:26:29 +0200

    Added end of string (\z) to regexes.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -18,9 +18,9 @@ class ProfilesController < ApplicationController
     
     # Find Facebook if from url (id param) by using regexes
     profile_id = case params[:id]
-    when /[a-z\/:\.-]+\.fbcdn\.net\/[\w\/\-]+\/\w(\d+)_[\d\_]+.jpg/ then "#{$1}"
-    when /[a-z\/:\.-]+\.fbcdn\.net\/[\w\/\-\.]+\/\d*_?\d+_(\d+)_\d+_[\d_]*\w.jpg/ then "#{$1}"
-    when /[a-z\/:\.]*\.facebook\.com\/[\w\W]+\.php\?pid=\d+&id=(\d+)(&fbid=\d+)?/ then "#{$1}"
+    when /[a-z\/:\.-]+\.fbcdn\.net\/[\w\/\-]+\/\w(\d+)_[\d\_]+.jpg\z/ then "#{$1}"
+    when /[a-z\/:\.-]+\.fbcdn\.net\/[\w\/\-\.]+\/\d*_?\d+_(\d+)_\d+_[\d_]*\w.jpg\z/ then "#{$1}"
+    when /[a-z\/:\.]*\.facebook\.com\/[\w\W]+\.php\?pid=\d+&id=(\d+)(&fbid=\d+)?\z/ then "#{$1}"
     
     # No matches, show error
     else
