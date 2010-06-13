b3a1aedc2ad0608d86e62962611f7b0519460806
Runar <runar@me.com>
2010-06-13 20:00:16 +0200

    Commentary changes and minor clean up.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,31 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
   
+  # Run set_locale before every controller
   before_filter :set_locale
   
   # Set current locale based on browser settings
-  def set_locale
-  
-    # params[:preferred_language_from] = request.preferred_language_from([:en])
-    # # nil
-    # 
-    # params[:available_locales] = I18n.available_locales
-    # # [:en, :no, :de]
-    # 
-    # params[:locale] = request.compatible_language_from(I18n.available_locales)
-    # # nil
-    # 
-    # params[:user_preferred_languages] = request.user_preferred_languages
-    # # ["en-US"]
-    # 
-    # request.user_preferred_languages
-    # # => [ 'en-GB']
-    # 
-    # available = ["en", "en-GB"]
-    # params[:test] = request.compatible_language_from(available)
-    
+  def set_locale    
     I18n.locale = request.compatible_language_from(I18n.available_locales) unless params[:locale]
-    
   end
   
   # Set locale option on all urls
