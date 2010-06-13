4b986181115410ea520a45df125d997ceaf6dc51
Runar <runar@me.com>
2010-06-13 03:33:31 +0200

    Routes moved to scope to support languages via url.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,23 @@
 Facefind::Application.routes.draw do |map|
   
+  # Scope including /locale
+  scope "/:locale" do
+    # Find profile route
+    match '/find(.:format)/(:id)' => "profiles#find", :id => /.+/, :as => :find
+
+    # Search route
+    match '/search' => "profiles#search", :as => :search
+
+    # About route
+    match '/about' => "profiles#about", :as => :about
+  end
+  
   # Find profile route
-  match '/find(.:format)/(:id)' => "profiles#find", :id => /.+/, :as => :find
+  match '/(:locale/)find(.:format)/(:id)' => "profiles#find", :id => /.+/, :as => :find
   
-  # Search route
-  match '/search' => "profiles#search", :as => :search
-  
-  # About route
-  match '/about' => "profiles#about", :as => :about
+  # Home route
+  match '/:locale' => 'profiles#index', :as => :home
   
   # Root route
   root :to => "profiles#index"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
